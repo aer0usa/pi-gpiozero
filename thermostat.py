b72f41c3c9a1c9d5cead7d58e6d1b04c5f69ccb3
Eric Rinehart <aer0usa@gmail.com>
2018-05-13 22:17:18 +0000

    Added one-off detection of ADC values for real-time thermostat function.

--- thermostat.py
+++ thermostat.py
@@ -1,9 +1,22 @@
 from gpiozero import LED, MCP3008
 from time import sleep
 
+adc = MCP3008(channel=1)
+pot = MCP3008(channel=0)
+led = LED(21)
+currentTemp = 80.0
+thermostat_setting = 80.0
+
 def convert_temp(gen):
     for value in gen:
         yield (value * 3.3 - 0.5) * 100
+
+def immediate_temp(rawVal):
+    return (rawVal * 3.3 - 0.5) * 100
+
+# Set thermostat from 50 to 90 degrees
+def thermostat_value(rawVal):
+    return ((rawVal * 40) + 50)
 
 def fahrenheit(celcius=22):
     return (celcius * (9/5) + 32)
@@ -14,11 +27,9 @@ def activate(state=False):
     else:
         led.off()
 
-adc = MCP3008(channel=1)
-led = LED(21)
-target = 80.0
-
-for temp in convert_temp(adc.values):
-    print('The temperature is', round(temp, 1), 'C, ', round(fahrenheit(temp), 1), 'F')
-    activate(fahrenheit(temp) < target)
+while True:
+    currentTemp = round(fahrenheit(immediate_temp(adc.value)), 1)
+    thermostat_setting = round(thermostat_value(pot.value), 1)
+    print('Temp ', currentTemp, ' / ', thermostat_setting, ' F')
+    activate(currentTemp < thermostat_setting)
     sleep(1)
